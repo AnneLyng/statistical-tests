6d87ece66f3eb89f5eb1f5d2b1bc6fca47482b5f
Anne Lyngholm <als@sund.ku.dk>
2018-05-28 16:13:39 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -2,6 +2,11 @@
 ### Description: The server part of the t-test shiny app
 
 server <- function(input, output, session){
+  
+  v <- reactiveValues(doTest = FALSE)
+  testMethod <<- "oneSample"
+  dataChoice <<- "select"
+  
  output$dataLoad <- renderText({
    "Please insert data in either .txt or .csv file format.<br>Notice that the upload may give you an error, 
 if the the separator is incorrect.
@@ -9,9 +14,17 @@ if the the separator is incorrect.
    and choose the correct separator for your data."
  })
  
+ output$introWilcox <- renderText({
+   "This is an introduction to Wilcox - and it is not very good."
+ })
+ 
+ output$dataChooseIntro <- renderText({
+   "Please select data fitting to your problem. <br>There are three different datasets to select from."
+ })
+ 
  output$steps <- renderText({
    "To compute a t-test using this plug-in, you need to do the following steps. 
-    <br><ol><li>Upload your data in the box below</li>
+    <br><ol><li>Upload your data in the box below <b>OR</b> choose one of the existing datasets</li>
    <li>Ensure that the structure of the data is correct using the Visualization of a subset of data-box</li>
    <li>Choose the t-test that suits the hypothesis you wish to test</li>
    <li>Agree to the assumptions</li>
@@ -23,11 +36,65 @@ if the the separator is incorrect.
    "We will present examples of the three types of t-test"
  })
  
+ output$dataChoose <- renderText({
+   "Use the visulization of your data to ensure that you are using the correct data."
+ })
+ 
  output$dataLoadText <- renderText({
    "Use the visulization of your data to ensure that your data is read correctly. <br>
      The data should be divided into columns. If not try to select another separator in the 'Choose separator' dropdown."
  })
+ 
+ observe({
+   shinyjs::hide(id = "uploadData")
+   shinyjs::hide(id = "showUploadData")
+   shinyjs::hide(id = "selectData")
+   shinyjs::hide(id = "showSelectData")
+ })
+ 
+ observeEvent(input$uploadButton, {
+   if(input$uploadButton %% 2 != 1){
+     shinyjs::hide(id = "uploadData")
+     shinyjs::hide(id = "showUploadData")
+   } else {
+     shinyjs::show(id = "uploadData")
+     shinyjs::show(id = "showUploadData")
+     shinyjs::hide(id = "selectData")
+     shinyjs::hide(id = "showSelectData")
+     dataChoice <<- "upload"
+     if(exists("ttest_data")){
+       rm(ttest_data, pos=".GlobalEnv")
+     }
+   }})
+ 
+ observeEvent(input$selectButton, {
+   if(input$selectButton %% 2 != 1){
+     shinyjs::hide(id = "selectData")
+     shinyjs::hide(id = "showSelectData")
+   } else {
+     shinyjs::show(id = "selectData")
+     shinyjs::show(id = "showSelectData")
+     shinyjs::hide(id = "uploadData")
+     shinyjs::hide(id = "showUploadData")
+     dataChoice <<- "select"
+   }})
 
+ datasetInput <- reactive({
+   switch(input$dataset,
+          
+          adi <- read.csv2("http://192.38.117.59/~tag/Teaching/share/data/adi.csv"),
+          "differences_between_differences" = as.data.frame(cbind(control = ts(adi[adi$training=="CON","saa.post"]-
+                                                                                 adi[adi$training=="CON","saa.pre"]),
+                                                                  high = ts(adi[adi$training=="HIGH","saa.post"]-
+                                                                           adi[adi$training=="HIGH","saa.pre"]))),
+          "compare_train_group" = as.data.frame(cbind(control = ts(adi[adi$training=="CON","saa.post"]),
+                                                      high = ts(adi[adi$training=="HIGH","saa.post"]))),
+          "compare_pre_post_interven" = adi[adi$sex=="0",8:9])
+ })
+ 
+ output$contentsData <- renderTable({
+   head(datasetInput())
+ })
  
  output$contents <- renderTable({
    
@@ -38,6 +105,7 @@ if the the separator is incorrect.
    if(exists("ttest_data")){
      rm(ttest_data, pos=".GlobalEnv")
    }
+   
    req(input$file1)
    
    # when reading semicolon separated files,
@@ -64,9 +132,6 @@ if the the separator is incorrect.
    in the left may provide the information that you need."
  })
  
- v <- reactiveValues(doTest = FALSE)
- dataCheck <<- 0
- testMethod <<- "oneSample"
  
  observeEvent(input$oneSample, 
               {output$assumptions <- renderText({paste("<h4>You have choosen the <font color='#0080ff'>one sample</font>
@@ -132,8 +197,7 @@ variables are required, besides this, the test comes with the following assumpti
    })}
  
  observeEvent(input$understand,
-              {if(exists("ttest_data")){
-                  dataCheck <<- 1
+              {if(dataChoice == "upload" & exists("ttest_data")){
                   output$class <- renderText({if(v$doTest == FALSE) return()
                                               isolate({"Please supply the needed information below:"})})
                   if(testMethod=="oneSample"){
@@ -147,6 +211,7 @@ variables are required, besides this, the test comes with the following assumpti
                       isolate({numericInput("varInterest2", "Test value:", 0, width=200)})
                       })
                     output$result <- renderPrint({if(v$doTest == FALSE) return()})
+                    session$sendCustomMessage(type = "scrollCallback", 1)
                   } else if(testMethod=="twoSample"){
                     output$headArg <- renderText({if(v$doTest == FALSE) return()
                       isolate({"<h4>Arguments in the <font color='#0080ff'>two sample</font> t-test:</h4>"})})
@@ -159,6 +224,7 @@ variables are required, besides this, the test comes with the following assumpti
                                   selected = NULL, multiple = FALSE,
                                   selectize = TRUE, width = 200, size = NULL)})})
                     output$result <- renderPrint({if(v$doTest == FALSE) return()})
+                    session$sendCustomMessage(type = "scrollCallback", 1)
                   } else if(testMethod=="paired"){
                     output$headArg <- renderText({if(v$doTest == FALSE) return()
                       isolate({"<h4>Arguments in the <font color='#0080ff'>paired</font> t-test:</h4>"})})
@@ -171,14 +237,57 @@ variables are required, besides this, the test comes with the following assumpti
                                   selected = NULL, multiple = FALSE,
                                   selectize = TRUE, width = 200, size = NULL)})})
                     output$result <- renderText({if(v$doTest == FALSE) return()})
+                    session$sendCustomMessage(type = "scrollCallback", 1)
                     }
-              } else {
-                dataCheck <<- 0
+              } else if(dataChoice == "select"){
+                output$class <- renderText({if(v$doTest == FALSE) return()
+                  isolate({"Please supply the needed information below:"})})
+                if(testMethod=="oneSample"){
+                  output$headArg <- renderText({if(v$doTest == FALSE) return()
+                    isolate({"<h4>Arguments in the <font color='#0080ff'>one sample</font> t-test:</h4>"})})
+                  output$arguments1 <- renderUI({if(v$doTest == FALSE) return()
+                    isolate({selectInput("varInterest1", "Sample of interest:", colnames(datasetInput()),
+                                         selected = NULL, multiple = FALSE,
+                                         selectize = TRUE, width = 200, size = NULL)})})
+                  output$arguments2 <- renderUI({if(v$doTest == FALSE) return()
+                    isolate({numericInput("varInterest2", "Test value:", 0, width=200)})
+                  })
+                  output$result <- renderPrint({if(v$doTest == FALSE) return()})
+                  session$sendCustomMessage(type = "scrollCallback", 1)
+                } else if(testMethod=="twoSample"){
+                  output$headArg <- renderText({if(v$doTest == FALSE) return()
+                    isolate({"<h4>Arguments in the <font color='#0080ff'>two sample</font> t-test:</h4>"})})
+                  output$arguments1 <- renderUI({if(v$doTest == FALSE) return()
+                    isolate({selectInput("varInterest1", "Sample 1 of interest:", colnames(datasetInput()),
+                                         selected = NULL, multiple = FALSE,
+                                         selectize = TRUE, width = 200, size = NULL)})})
+                  output$arguments2 <- renderUI({if(v$doTest == FALSE) return()
+                    isolate({selectInput("varInterest2", "Sample 2 of interest:", colnames(datasetInput()),
+                                         selected = NULL, multiple = FALSE,
+                                         selectize = TRUE, width = 200, size = NULL)})})
+                  output$result <- renderPrint({if(v$doTest == FALSE) return()})
+                  session$sendCustomMessage(type = "scrollCallback", 1)
+                } else if(testMethod=="paired"){
+                  output$headArg <- renderText({if(v$doTest == FALSE) return()
+                    isolate({"<h4>Arguments in the <font color='#0080ff'>paired</font> t-test:</h4>"})})
+                  output$arguments1 <- renderUI({if(v$doTest == FALSE) return()
+                    isolate({selectInput("varInterest1", "Sample 1 of interest:", colnames(datasetInput()),
+                                         selected = NULL, multiple = FALSE,
+                                         selectize = TRUE, width = 200, size = NULL)})})
+                  output$arguments2 <- renderUI({if(v$doTest == FALSE) return()
+                    isolate({selectInput("varInterest2", "Sample 2 of interest:", colnames(datasetInput()),
+                                         selected = NULL, multiple = FALSE,
+                                         selectize = TRUE, width = 200, size = NULL)})})
+                  output$result <- renderText({if(v$doTest == FALSE) return()})
+                  session$sendCustomMessage(type = "scrollCallback", 1)
+                }
+              }
+                else {
                   output$class <- renderText({if(v$doTest == FALSE) return()
                                               isolate({"Data is nessecary to compute a t-test. Load the data in the 'Upload data' box"})})
                   output$result <- renderText({if(v$doTest == FALSE) return()})
-              }
-                session$sendCustomMessage(type = "scrollCallback", 1)}
+                  session$sendCustomMessage(type = "scrollCallback", 1)
+              }}
                 )
  
  observeEvent(input$understand, {
@@ -210,7 +319,11 @@ outputText <- "P-value of: "
                 if(testMethod == "oneSample"){
                   myval1 <- testReact1()
                   myval2 <- testReact2()
-                  vec <- eval(parse(text=paste("ttest_data$",myval1,sep="")))
+                  if(dataChoice == "upload"){
+                    vec <- eval(parse(text=paste("ttest_data$",myval1,sep="")))
+                  } else {
+                    vec <- eval(parse(text=paste("datasetInput()$",myval1,sep="")))
+                    }
                   pval <- t.test(x=vec,mu=myval2)$p.value
                   if(pval < 0.0001) {pval <- "< 0.0001"
                   } else {
@@ -221,11 +334,17 @@ outputText <- "P-value of: "
                     isolate({paste("<br><br><h4>Results</h4><hr>",
                                    outputText, pval, ". And a 95% confidence interval of ", round(intConf[1],4), "; ", 
                                    round(intConf[2],4), ".", sep="")})})
+                  session$sendCustomMessage(type = "scrollCallback", 1)
                 } else if(testMethod=="twoSample"){
                   myval1 <- testReact1()
                   myval2 <- testReact2()
-                  vec1 <- eval(parse(text=paste("ttest_data$",myval1,sep="")))
-                  vec2 <- eval(parse(text=paste("ttest_data$",myval2,sep="")))
+                  if(dataChoice == "upload"){
+                    vec1 <- eval(parse(text=paste("ttest_data$",myval1,sep="")))
+                    vec2 <- eval(parse(text=paste("ttest_data$",myval2,sep="")))
+                  } else {
+                    vec1 <- eval(parse(text=paste("datasetInput()$",myval1,sep="")))
+                    vec2 <- eval(parse(text=paste("datasetInput()$",myval2,sep="")))
+                  }
                   pval <- t.test(x=vec1,y=vec2)$p.value
                   if(pval < 0.0001) {pval <- "< 0.0001"
                   } else {
@@ -236,11 +355,17 @@ outputText <- "P-value of: "
                     isolate({paste("<br><br><h4>Results</h4><hr>",outputText, round(pval,4), ". And a 95% confidence interval of ",
                                    round(intConf[1],4), 
                                    "; ", round(intConf[2],4), ".", sep="")})})
+                  session$sendCustomMessage(type = "scrollCallback", 1)
                 } else {
                   myval1 <- testReact1()
                   myval2 <- testReact2()
-                  vec1 <- eval(parse(text=paste("ttest_data$",myval1,sep="")))
-                  vec2 <- eval(parse(text=paste("ttest_data$",myval2,sep="")))
+                  if(dataChoice == "upload"){
+                    vec1 <- eval(parse(text=paste("ttest_data$",myval1,sep="")))
+                    vec2 <- eval(parse(text=paste("ttest_data$",myval2,sep="")))
+                  } else {
+                    vec1 <- eval(parse(text=paste("datasetInput()$",myval1,sep="")))
+                    vec2 <- eval(parse(text=paste("datasetInput()$",myval2,sep="")))
+                  }
                   pval <- t.test(x=vec1,y=vec2)$p.value
                   if(pval < 0.0001) {pval <- "< 0.0001"
                   } else {
@@ -251,6 +376,7 @@ outputText <- "P-value of: "
                     isolate({paste("<br><br><h4>Results</h4><hr>",outputText, round(pval,4), ". And a 95% confidence interval of  ",
                                                       round(intConf[1],4),
                                    "; ", round(intConf[2],4), ".", sep="")})})
+                  session$sendCustomMessage(type = "scrollCallback", 1)
                 }
               })
  
--- ui.R
+++ ui.R
@@ -9,16 +9,16 @@ library(shinyjs)
 
 # the ui
 dashboardPage(
-  dashboardHeader(title="t-test plug-in"),
+  dashboardHeader(title="statistical tests"),
   dashboardSidebar(sidebarMenu(
-    menuItem("t-test", tabName = "t-test"),
+    menuItem("tests", tabName = "tests"),
     menuItem("examples", tabName = "examples"),
     menuItem("theory", tabName="theory")
   )
   ),
   dashboardBody(
     tabItems(
-      tabItem("t-test",
+      tabItem("tests",
               tags$head(
                 # Some css to style the div to make it more easily visible
                 tags$style(
@@ -43,70 +43,102 @@ dashboardPage(
       );'
                 )
               ),
-              # The info box aka how-to
               fluidRow(
                 column(width=12,
                        box(width = NULL, solidHeader = TRUE,
                            status = "warning",
-                           title="Steps to do a t-test using this app",
-                           htmlOutput("steps"))
-                       )),
+                           title="Steps to do a statistical test using this app",
+                           htmlOutput("steps"),
+                           actionButton("selectButton", "Select existing data set"),
+                           actionButton("uploadButton", "Use own data set"))
+                )),
+              # The select data-box
+              fluidRow(
+                column(width=5,
+                       box(id = "selectData", width = NULL, #height = 350, 
+                           status = "info",
+                           h4("Choose data"),
+                           hr(),
+                           htmlOutput("dataChooseIntro"),
+                           br(),
+                           selectInput(inputId = "dataset",
+                                       label = "Choose a dataset:",
+                                       choices = c("differences_between_differences", "compare_train_group",
+                                                   "compare_pre_post_interven"),
+                                       width=300))
+                ),
+                column(width=7,
+                       box(id="showSelectData", width=NULL, #height = 350,
+                           status = "info",
+                           h4("Visualization of a subset of data (existing)"),
+                           hr(),
+                           htmlOutput("dataChoose"),
+                           br(),
+                           tableOutput("contentsData")))),
               # The upload data box
               fluidRow(
                 column(width=5,
-                       box(width=NULL, height = 425,
+                       box(id= "uploadData", width=NULL, #height = 425,
+                           status = "info",
                            h4("Upload data"),
-                           tags$hr(),
+                           hr(),
                            htmlOutput("dataLoad"),
                            br(),
                            fileInput("file1", "Choose a .csv or .txt file:",
-                                  multiple = FALSE,
-                                  accept = c("text/csv",
-                                             "text/comma-separated-values,text/plain",
-                                             ".csv")),
-                        selectInput("sep", "Choose separator:", c(comma=",", semicolon=";", tab = "\t", whitespace =" "),
-                                    selected = NULL, multiple = FALSE,
-                                    selectize = TRUE, width = 200, size = NULL),
-                        checkboxInput("header", "Does the data have named columns (header)?", TRUE))),
+                                     multiple = FALSE,
+                                     accept = c("text/csv",
+                                                "text/comma-separated-values,text/plain",
+                                                ".csv")),
+                           selectInput("sep", "Choose separator:", c(comma=",", semicolon=";", tab = "\t", whitespace =" "),
+                                       selected = NULL, multiple = FALSE,
+                                       selectize = TRUE, width = 200, size = NULL),
+                           checkboxInput("header", "Does the data have named columns (header)?", TRUE))),
                 # The data visualization box
                 column(width=7, 
-                       box(width=NULL, height = 425,
-                           h4("Visualization of a subset of data"),
+                       box(id="showUploadData",
+                           width=NULL, #height = 425,
+                           status = "info",
+                           h4("Visualization of a subset of data (upload)"),
                            tags$hr(),
                            htmlOutput("dataLoadText"),
                            br(),
                            tableOutput("contents")))),
-              # The t-test box
-              useShinyjs(),
-              fluidRow(
-                column(width=12,
-                       box(width = NULL, 
-                           h4("Choice of t-test and assumptions"),
-                           hr(),
-                           textOutput("testSelector"),
-                           br(),
-                           actionButton("oneSample", "One Sample"),
-                           actionButton("twoSample", "Two Sample"),
-                           actionButton("paired", "Paired Sample"),
-                           htmlOutput("assumptions"),
-                           actionButton("understand", "I understand the assumptions and the assumptions are fulfilled"),
-                           br(),
-                           br(),
-                           textOutput("class"),
-                           br(),
-                           br(),
-                           htmlOutput("headArg"),
-                           hr(),
-                           fluidRow(width=12,
-                                    column(width=2,
-                                           uiOutput("arguments1")),
-                                    column(width=2,
-                                           uiOutput("arguments2")),
-                                    column(width=2,
-                                           br(),
-                                           actionButton("startTTest", "Compute t-test"))),
-                           htmlOutput("result")
-                           ))
+              tabsetPanel(type = "tabs",
+                          tabPanel("t-tests",
+                                   # The t-test box
+                                   useShinyjs(),
+                                   fluidRow(
+                                     column(width=12,
+                                            box(width = NULL, 
+                                                h4("Choice of t-test and assumptions"),
+                                                hr(),
+                                                textOutput("testSelector"),
+                                                br(),
+                                                actionButton("oneSample", "One Sample"),
+                                                actionButton("twoSample", "Two Sample"),
+                                                actionButton("paired", "Paired Sample"),
+                                                htmlOutput("assumptions"),
+                                                actionButton("understand", "I understand the assumptions and the assumptions are fulfilled"),
+                                                br(),
+                                                br(),
+                                                textOutput("class"),
+                                                br(),
+                                                br(),
+                                                htmlOutput("headArg"),
+                                                hr(),
+                                                fluidRow(width=12,
+                                                         column(width=2,
+                                                                uiOutput("arguments1")),
+                                                         column(width=2,
+                                                                uiOutput("arguments2")),
+                                                         column(width=2,
+                                                                br(),
+                                                                actionButton("startTTest", "Compute t-test"))),
+                                                htmlOutput("result")
+                                            ))
+                                   )),
+                          tabPanel("Wilcox", textOutput("introWilcox")),
+                          tabPanel("Linear", tableOutput("table"))
               )),
       tabItem("examples",
               titlePanel("Examples of app's three t-tests"),
